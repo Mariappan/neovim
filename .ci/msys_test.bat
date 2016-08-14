e5b2cee8518a2cbeebe60c43541e0652c91c1d51
Rui Abreu Ferreira <raf-ep@gmx.com>
2016-08-15 00:01:04 +0100

    Appveyor: Enable functional tests
    Most functional tests don't work on Windows yet, for now enable
    a subset of the tests in Appveyor builds.
    
    Busted output is set to JUnit and the test results are uploaded to
    the Appveyor test console.

---
+++ .ci/msys_test.bat
@@ -0,0 +1,7 @@
+:: FIXME(equalsraf) currently not all tests work on Windows
+:: for now just run what we can
+mingw32-make functionaltest TEST_FILE=test\functional\eval\printf_spec.lua VERBOSE=1 || goto :error
+
+goto :EOF
+:error
+exit /b %errorlevel%
